ed576681a23972f268d582f8902675225bbee094
Sergio <santozzi@gmail.com>
2025-01-24 02:27:27 -0300

    agrego cartel al enviar subscripcion valida

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -6,6 +6,7 @@ import { ButtonPrimary } from '../utils/buttons/ButtonPrimary';
 import {useForm} from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';	
 import { formSchema } from '../../validations/formSchema';
+import { useState } from 'react';
 
 
 const Subscribe: React.FC  = () => {
@@ -13,16 +14,20 @@ const Subscribe: React.FC  = () => {
 	const { register,handleSubmit,formState:{errors}} = useForm({
 		resolver: zodResolver(formSchema),
 	});
-    handleSubmit(data=>{
-	console.log(data)
-		
-	})
+    const [validated, setValidated] = useState(false);
 	return (
 		<div className='form-container'>
-
+             
 				<img src={logo} alt="logo"/>
+				{!validated ?<>
 			  	<h1>subscribe</h1>
-				<form onSubmit={handleSubmit(data=>console.log(data))}>
+				<form onSubmit={handleSubmit(() => {
+					if (errors.email) {
+						setValidated(false);
+					} else {
+						setValidated(true);
+					}
+				})}>
 					<input 
 					   style={{borderColor: errors.email && 'red'}}
 					
@@ -31,12 +36,13 @@ const Subscribe: React.FC  = () => {
 			
 					   {...register('email', {required: true})} /> 
 				
-						{errors.email?.message && <p>{errors.email.message.toString()}</p>}
+						
 					  
 					 
 					<ButtonPrimary title={'subscribe'}/>
 			    </form>
-				
+				</>
+				: <p className='subscribed'>Thank you for subscribing</p>}
 			
  		</div>
 	);
